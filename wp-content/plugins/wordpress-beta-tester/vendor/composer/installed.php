7a339e6b95c8580671e4b59843a5479bab72cff2
Chris Reynolds <chris@jazzsequence.com>
2022-01-07 15:25:52 -0700

    add beta tester plugin to easily enable latest nightly/rc

---
+++ wp-content/plugins/wordpress-beta-tester/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '05598edcafc32b63f5e924338fd8d85c869cf636',
+        'name' => 'afragen/wordpress-beta-tester',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'afragen/wordpress-beta-tester' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '05598edcafc32b63f5e924338fd8d85c869cf636',
+            'dev_requirement' => false,
+        ),
+        'wp-cli/wp-config-transformer' => array(
+            'pretty_version' => 'v1.2.8',
+            'version' => '1.2.8.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/wp-config-transformer',
+            'aliases' => array(),
+            'reference' => '0bb2b9162c38ca72370380aea11dc06e431e13a5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
