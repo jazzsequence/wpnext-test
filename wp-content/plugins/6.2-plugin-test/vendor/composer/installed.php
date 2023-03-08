3a9ea37ce21489b9554641163b54cca355ea5b51
Chris Reynolds <chris@jazzsequence.com>
2023-03-08 15:26:46 -0700

    adds cmb2 to 6.2 move_dir tests

---
+++ wp-content/plugins/6.2-plugin-test/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jazzsequence/6.2-plugin-test',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '3ae596b4257e649b69db84914258225042f52ad8',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cmb2/cmb2' => array(
+            'pretty_version' => 'v2.10.1',
+            'version' => '2.10.1.0',
+            'reference' => '4afc4bb7b92ab6d93aac2247c9a84af773e42532',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../cmb2/cmb2',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'jazzsequence/6.2-plugin-test' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '3ae596b4257e649b69db84914258225042f52ad8',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
