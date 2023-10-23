de87d46b3ccd2af1a8f07a0f69f37d9fd934ac6f
Chris Reynolds <chris@jazzsequence.com>
2023-10-23 09:12:39 -0600

    add a fake mu-plugin to pass the test

---
+++ wp-content/mu-plugins/pantheon.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Pantheon
+ * Plugin URI: https://pantheon.io/
+ * Description: Building on Pantheon's and WordPress's strengths, together.
+ * Version: 1.2.0
+ * Author: Pantheon
+ * Author URI: https://pantheon.io/
+ *
+ * @package pantheon
+ */
