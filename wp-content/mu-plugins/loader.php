f7ca8ceb655bdf955e748a9e6a2a061d8fda3abe
Chris Reynolds <chris@jazzsequence.com>
2023-10-23 09:53:20 -0600

    add the plugin header to the loader file so it will appear as the mu-plugin for behat tests

--- wp-content/mu-plugins/loader.php
+++ wp-content/mu-plugins/loader.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Plugin Name: Pantheon
+ * Plugin URI: https://pantheon.io/
+ * Description: Building on Pantheon's and WordPress's strengths, together.
+ * Version: 1.2.0
+ * Author: Pantheon
+ * Author URI: https://pantheon.io/
+ *
+ * @package pantheon
+ */
 $_mu_plugins = [
 	'pantheon-mu-plugin/pantheon.php',
 ];
