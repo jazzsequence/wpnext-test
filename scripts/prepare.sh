575a7d566faa8d591244f2e47d3c537dc044f8c8
Chris Reynolds <chris@jazzsequence.com>
2023-10-23 09:37:02 -0600

    prepare the plugins before a behat test

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -15,10 +15,19 @@ fi
 
 # Only run multidev creation if the TERMINUS_ENV is 'behat'.
 if [ "$TERMINUS_ENV" == 'behat' ]; then
-	###
 	# Create a new environment for this particular test run.
-	###
 	terminus env:create $TERMINUS_SITE.test-base $TERMINUS_ENV
+else
+	# If the environment was specified, make sure it exists.
+	$env_exists=$(terminus env:info $SITE_ENV )
+	if [ -z "$env_exists" ]; then
+		echo "Environment $TERMINUS_ENV does not exist."
+		exit 1
+	fi
+
+	# If it does exist, make sure there are no plugins that the tests don't expect.
+	terminus wp $SITE_ENV -- plugin delete --all
+	terminus wp $SITE_ENV -- plugin install akismet hello-dolly
 fi
 
 terminus env:wipe $SITE_ENV --yes
