e6bada6733450e6877452106b989e4f4961e3914
Chris Reynolds <chris@jazzsequence.com>
2024-02-06 11:04:46 -0700

    update pantheon mu plugin

--- wp-content/mu-plugins/pantheon-mu-plugin/inc/fonts.php
+++ wp-content/mu-plugins/pantheon-mu-plugin/inc/fonts.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Pantheon mu-plugin customizations to the WP Font Library.
+ * Pantheon mu-plugin customizations to the WP_Font_Library added in WordPress 6.5
  *
  * @package pantheon
  */
@@ -9,39 +9,54 @@
 namespace Pantheon\Fonts;
 
 /**
- * Modify the fonts directory.
- *
- * By default, this is set to true, so we can override the default fonts directory from wp-content/fonts to wp-content/uploads/fonts.
- *
- * Use the filter to set to false and use the default WordPress behavior (committing fonts to your repository and pushing from dev -> test -> live).
- *
- * @param bool $modify Whether to modify the fonts directory.
+ * Kick off our customizations to the WP_Font_Library.
  */
-define( 'PANTHEON_MODIFY_FONTS_DIR', apply_filters( 'pantheon_modify_fonts_dir', true ) );
-
-if ( PANTHEON_MODIFY_FONTS_DIR ) {
-	// Use the new fonts_dir filter added in WordPress 6.5. See https://github.com/WordPress/gutenberg/pull/57697.
-	if ( ! class_exists( 'WP_Font_Library' ) ) {
-		require WP_PLUGIN_DIR . '/gutenberg/lib/experimental/fonts/font-library/class-wp-font-library.php';
+function bootstrap() {
+	if ( pantheon_modify_fonts_dir() ) {
+		// Use the new font_dir filter added in WordPress 6.5. See https://github.com/WordPress/gutenberg/pull/57697.
+		add_filter( 'font_dir', __NAMESPACE__ . '\\pantheon_font_dir', 9 );
 	}
-	$defaults = \WP_Font_Library::fonts_dir();
-	pantheon_wp_fonts_dir( apply_filters( 'fonts_dir', $defaults ) );
-	add_filter( 'fonts_dir', 'pantheon_wp_fonts_dir' );
+}
+add_action( 'init', __NAMESPACE__ . '\\bootstrap' );
+
+/**
+ * Get the value of the pantheon_modify_fonts_dir filter.
+ * By default, this should return true (we're filtering).
+ *
+ * @return bool Whether to modify the fonts directory.
+ */
+function pantheon_modify_fonts_dir() {
+	/**
+	 * Modify the fonts directory.
+	 *
+	 * By default, this is set to true, so we can override the default fonts directory from wp-content/fonts to wp-content/uploads/fonts.
+	 *
+	 * Use the filter to set to false and use the default WordPress behavior (committing fonts to your repository and pushing from dev -> test -> live).
+	 *
+	 * @param bool $modify_fonts_dir Whether to modify the fonts directory.
+	 */
+	return apply_filters( 'pantheon_modify_fonts_dir', true );
 }
 
 /**
  * Define a custom font directory for the WP Font Library.
+ * Default to {WP_CONTENT_DIR}/uploads/fonts.
+ *
+ * @param array $defaults The default settings for the font directory.
  */
-function pantheon_wp_fonts_dir( $defaults ) {
-	var_dump( $defaults ); exit;
+function pantheon_font_dir( $defaults ) {
 	$wp_upload_dir = wp_get_upload_dir();
-	$uploads = $wp_upload_dir['basedir'];
+	$uploads_basedir = $wp_upload_dir['basedir'];
+	$uploads_baseurl = $wp_upload_dir['baseurl'];
 
-	/**
-	 * Filter the fonts directory.
-	 *
-	 * By default, this is set to wp-content/uploads/fonts. Use this filter to set the fonts directory to any other location.
-	 */
-	$fonts_dir = apply_filters( 'pantheon_wp_fonts_dir', $uploads . '/fonts' );
-	return $fonts_dir;
+	// Set our font directory.
+	$font_dir = $uploads_basedir . '/fonts';
+	$font_url = $uploads_baseurl . '/fonts';
+
+	$defaults['path'] = $font_dir;
+	$defaults['url'] = $font_url;
+	$defaults['basedir'] = $font_dir;
+	$defaults['baseurl'] = $font_url;
+
+	return $defaults;
 }
