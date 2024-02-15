c707ee6afa4062aa0fdb49bb37400d2652b60d62
Chris Reynolds <chris@jazzsequence.com>
2024-02-15 09:07:40 -0700

    Updating WordPress plugins

--- wp-content/plugins/gutenberg/gutenberg.php
+++ wp-content/plugins/gutenberg/gutenberg.php
@@ -5,7 +5,7 @@
  * Description: Printing since 1440. This is the development plugin for the block editor, site editor, and other future WordPress core functionality.
  * Requires at least: 6.3
  * Requires PHP: 7.0
- * Version: 17.6.2
+ * Version: 17.7.0
  * Author: Gutenberg Team
  * Text Domain: gutenberg
  *
@@ -13,8 +13,8 @@
  */
 
 ### BEGIN AUTO-GENERATED DEFINES
-define( 'GUTENBERG_VERSION', '17.6.2' );
-define( 'GUTENBERG_GIT_COMMIT', '8a04e27fd235bdb19e94b8898726599d3054ece6' );
+define( 'GUTENBERG_VERSION', '17.7.0' );
+define( 'GUTENBERG_GIT_COMMIT', '8d0e36d29f1d817c104f68f6032fae89a0ee7d1f' );
 ### END AUTO-GENERATED DEFINES
 
 gutenberg_pre_init();
