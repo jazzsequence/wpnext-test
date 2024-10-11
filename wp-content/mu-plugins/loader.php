f7b39be84cf921fea18f0f17d9b4f26b2beebda1
Chris Reynolds <chris@jazzsequence.com>
2024-10-11 10:47:06 -0600

    let's just pull cmb2 for now

--- wp-content/mu-plugins/loader.php
+++ wp-content/mu-plugins/loader.php
@@ -11,10 +11,10 @@
  */
 $_mu_plugins = [
 	'pantheon-mu-plugin/pantheon.php',
-	'cmb2/init.php',
+	// 'cmb2/init.php',
 	'git-updater/git-updater.php',
 ];
 
 foreach ( $_mu_plugins as $mu_plugin ) {
-	require_once $mu_plugin;
+	require_once dirname( __FILE__ ) . "/$mu_plugin";
 }
