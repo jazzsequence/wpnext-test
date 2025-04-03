551b031965bad5dcf8d2cdf9dae0e8b734a335ca
Chris Reynolds <chris@jazzsequence.com>
2025-04-03 15:10:10 -0600

    maybe switch to git mode or stay on sftp mode

--- scripts/wordpress-theme-update.sh
+++ scripts/wordpress-theme-update.sh
@@ -9,8 +9,18 @@ terminus env:commit wp59-test.dev --message="Updateing WordPress themes"
 # Wait for the workflow to finish
 terminus build:workflow:wait wp59-test.dev --max=30
 
-# Switch back to Git mode
-terminus connection:set wp59-test.dev git
+read -p "Switch back to git mode? (y or n): " -r GIT_MODE
+
+if [ $GIT_MODE == "y" ]; then
+  echo "Switching back to git mode."
+  terminus connection:set wp59-test.dev git
+elif [ $GIT_MODE == "n" ]; then
+  echo "Staying on SFTP mode."
+  exit 0
+else
+  echo "Invalid option. Please try again."
+  exit 1
+fi
 
 # Pull the changes
 git pull
