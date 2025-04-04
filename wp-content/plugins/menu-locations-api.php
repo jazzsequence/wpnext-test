3e67106a9ad67f64e96bb5d21d4e059fee52f348
Chris Reynolds <chris@jazzsequence.com>
2025-04-04 14:24:46 -0600

    add test for 6.8

---
+++ wp-content/plugins/menu-locations-api.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: 6.8 Test
+ * Description: Test  Menu thingie for Rest API
+ * Version: 1.0
+ * Author: Chris Reynolds
+ * Author URI: https://chrisreynolds.io
+ * License: MIT
+ */
+add_filter( 'rest_menu_read_access', '__return_true' );
