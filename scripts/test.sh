5f6d130e385d3c36ab25dd44a61133fb9cbc0359
Chris Reynolds <chris@jazzsequence.com>
2025-04-07 13:43:26 -0600

    don't delete if the environment exists

--- scripts/test.sh
+++ scripts/test.sh
@@ -17,22 +17,6 @@ export SITE_ENV="${TERMINUS_SITE}.${TERMINUS_ENV}"
 export WORDPRESS_ADMIN_USERNAME=testuser
 export WORDPRESS_ADMIN_PASSWORD=$(terminus secret:site:list $TERMINUS_SITE --format=json | jq -r '.testpass.value')
 
-# Don't delete anything if a specific multidev was passed.
-if [ -z "$MULTIDEV_ARG" ]; then
-    # Check for an existing behat multidev
-    PANTHEON_MULTIDEV_JSON=$(terminus multidev:list -n ${TERMINUS_SITE} --format=json)
-    if echo "${PANTHEON_MULTIDEV_JSON}" | jq -e --arg TERMINUS_ENV "$TERMINUS_ENV" 'has($TERMINUS_ENV)' > /dev/null; then
-        echo "Multidev environment $TERMINUS_ENV exists."
-        if [ "$SKIP_CLEANUP" == '1' ]; then
-            echo "Cleanup skipped. Leaving the existing $TERMINUS_ENV environment..."
-        else
-            terminus multidev:delete -y --delete-branch -- "$SITE_ENV"
-        fi
-    else
-        echo "Multidev environment $TERMINUS_ENV does not exist."
-    fi
-fi
-
 BEHAT_PATH="./vendor/pantheon-systems/pantheon-wordpress-upstream-tests"
 
 # Prepare the tests
